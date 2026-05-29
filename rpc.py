77567db65a726b92645d13ddbaf094e606f4e5cf
AllieSells <las8247@uncw.edu>
2026-05-28 21:52:29 -0400

    0.21.3.5 - Python is stupid and I hate it

--- rpc.py
+++ rpc.py
@@ -1,20 +1,25 @@
+from sys import exception
 from pypresence import Presence
 
-CLIENT_ID = "1509269648330194954" # RPC ID
+try:
 
-RPC = Presence(CLIENT_ID)
-RPC.connect()
+    CLIENT_ID = "1509269648330194954" # RPC ID
+
+    RPC = Presence(CLIENT_ID)
+    RPC.connect()
 
 
-def update(state: str = None, details: str = None):
-    try:
-        RPC.update(state=state, details=details, large_image="logo", large_text="Dungeons of Ærrok")
-    except Exception as e:
-        print(f"[WARNING]: Failed to update RPC: {e}")
+    def update(state: str = None, details: str = None):
+        try:
+            RPC.update(state=state, details=details, large_image="logo", large_text="Dungeons of Ærrok")
+        except Exception as e:
+            print(f"[WARNING]: Failed to update RPC: {e}")
 
-def get_status():
-    try:
-        return RPC.get()
-    except Exception as e:
-        print(f"[WARNING]: Failed to get RPC status: {e}")
-        return None
+    def get_status():
+        try:
+            return RPC.get()
+        except Exception as e:
+            print(f"[WARNING]: Failed to get RPC status: {e}")
+            return None
+except Exception as e:
+    print(f"[WARNING]: Failed to initialize RPC: {e}")
